c2d8a94c347f58aede618e23312213052c0df04d
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2023-06-21 14:26:04 +0530

    fix: fix env variables error

--- src/frontend/src/utils/github-url.ts
+++ src/frontend/src/utils/github-url.ts
@@ -1,10 +1,9 @@
-export function githubUrl(from:'typeof from'){
+export function githubUrl(){
     const rootUrl = 'https://github.com/login/oauth/authorize';
     const options = {
-        client_id: process.env.VUE_APP_GITHUB_OAUTH_CLIENT_ID,
-        redirect_uri: process.env.VUE_APP_GITHUB_OAUTH_REDIRECT_URL,
+        client_id: import.meta.env.VITE_APP_GITHUB_OAUTH_CLIENT_ID,
+        redirect_uri: import.meta.env.VITE_APP_GITHUB_OAUTH_REDIRECT_URL,
         scope: 'user:email',
-        state: from,
     }
     const qs : URLSearchParams = new URLSearchParams(options);
     return `${rootUrl}?${qs.toString()}`;
