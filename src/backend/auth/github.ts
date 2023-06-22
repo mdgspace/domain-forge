87a34b9d0adab13233e23b646e4600938889b20f
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2023-06-22 14:03:24 +0530

    fix: use strictly not equal operator

--- src/backend/auth/github.ts
+++ src/backend/auth/github.ts
@@ -4,7 +4,7 @@ async function githubAuth(ctx: Context, id: string, secret: string) {
   const code = ctx.request.url.searchParams.get("code");
   const rootUrl = new URL("https://github.com/login/oauth/access_token");
 
-  if (code != null) {
+  if (code !== null) {
     ctx.response.body = "authenticating...";
     rootUrl.search = new URLSearchParams({
       client_id: id,
