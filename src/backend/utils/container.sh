d8370f56dd087f5f0491d8d4aee781bf3c67b3ea
Pratham Agarwal <agarwalpratham1812@gmail.com>
2023-12-22 20:07:36 +0530

    feat: added dockerfile feature

--- src/backend/utils/container.sh
+++ src/backend/utils/container.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 if ! command -v git >/dev/null 2>&1; then
     echo "Git is not installed. Exiting."
     exit 1
@@ -11,32 +11,53 @@ if ! command -v docker >/dev/null 2>&1; then
     echo "docker not installed. Exiting"
     exit 1
 fi
-PORT_MIN=8000;
-PORT_MAX=9999;
+PORT_MIN=8000
+PORT_MAX=8099
 suffix=$(echo $RANDOM | md5sum | head -c 20)
 
 flag=$1
 name=$2-$suffix
 resource=$3
-# env=$4 
+env=$4 
+
+available_ports=()
+
+for ((port=PORT_MIN; port<=PORT_MAX; port++)); do
+    if ! ss -ln src :$port | grep -q "\<$port\>"; then
+        available_ports+=($port)
+    fi
+done
+
+echo "Available ports: ${available_ports[56]}"
+
+AVAILABLE=0
 if [ $flag = "-g" ]; then
     git clone $resource $name
-    sudo pack build $name --path $name --builder paketobuildpacks/builder-jammy-full 
-    AVAILABLE=$PORT_MIN
-    while [ $AVAILABLE -lt $PORT_MAX ]; do
-        result=$(ss -ln src :$AVAILABLE | grep -Ec -e "\<$AVAILABLE\>")
-        if [ "$result" -eq 0 ]; then
-            echo "Port $AVAILABLE is available";
-            sudo docker stop test-img
-            sudo docker rm test-img
-            sudo docker run -d --name test-img test
-            test=$(sudo docker exec -it test-img lsof -i -P -n | grep LISTEN | awk -F'[:(]' '{print $2}')
-            sudo docker stop test-img
-            sudo docker run -d --name $name -p $AVAILABLE:$test $name
-            echo "Container $name running on port $AVAILABLE"
-            break
-        fi        
-        AVAILABLE=$((AVAILABLE+1))
-    done
+    if [ -f "$name/docker-compose.yml" ]; then
+        echo "docker-compose.yml exists in $name"
+    elif [ -f "$name/Dockerfile" ]; then
+        echo "Dockerfile exists in $name"
+        sudo docker build -t $name $name
+        sudo docker stop test-img
+        sudo docker rm test-img
+        sudo docker run -d --name test-img $name
+        test=$(sudo docker exec -it test-img lsof -i -P -n | grep LISTEN | awk -F'[:(]' '{print $2}')
+        sudo docker stop test-img
+        sudo docker run -d --name $name -p ${available_ports[$AVAILABLE]}:$test $name
+        echo "Container $name running on port $AVAILABLE"
+        test=$(sudo docker exec -it test-img lsof -i -P -n | grep LISTEN | awk -F'[:(]' '{print $2}')
+    else
+        sudo pack build $name --path $name --builder paketobuildpacks/builder-jammy-full 
+        echo "Port ${available_ports[$AVAILABLE]} is available";
+        sudo docker stop test-img
+        sudo docker rm test-img
+        sudo docker run -d --name test-img $name
+        test=$(sudo docker exec -it test-img lsof -i -P -n | grep LISTEN | grep IPv6 | awk -F'[:(]' '{print $2}')
+        sudo docker stop test-img
+        sudo docker run -d --name $name -p ${available_ports[$AVAILABLE]}:$test $name
+        echo "Container $name running on port ${available_ports[$AVAILABLE]}"
+        AVAILABLE+=1
+    fi
     rm -rf $name
+    ./automate.sh -p ${available_ports[$AVAILABLE]} $name
 fi
