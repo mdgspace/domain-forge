a2867956fc0d4a917066db92fa139b78aa2a3094
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2024-02-09 23:37:59 +0530

    rfac: edit .gitignore and .dockerignore

--- src/backend/dependencies.ts
+++ src/backend/dependencies.ts
@@ -6,4 +6,5 @@ import {
 import { Session } from "https://deno.land/x/oak_sessions@v4.1.9/mod.ts";
 import { create, verify } from "https://deno.land/x/djwt@v2.9.1/mod.ts";
 import { exec } from "https://deno.land/x/exec/mod.ts";
+
 export { Application, Context, create, exec, Router, Session, verify };
