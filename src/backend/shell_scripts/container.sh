e21cacb65ab1c4330c8dce6d663f82030e54b40d
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2024-04-03 23:39:27 +0530

    rfac: fix server block indentation

--- src/backend/shell_scripts/container.sh
+++ src/backend/shell_scripts/container.sh
@@ -5,28 +5,7 @@ name=$2
 resource=$3
 exp_port=$4
 max_mem=$5 
-server_block="# Virtual Host configuration for $2
-    server {
-    listen 80;
-    listen [::]:80;
-    listen 443 ssl;
-    listen [::]:443 ssl;
-    server_name $2;
-    location / {
-        proxy_pass http://localhost:${available_ports[$AVAILABLE]};
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-    }
-    charset utf-8;
-    client_max_body_size 20M;
-    ssl_certificate /etc/letsencrypt/live/df.mdgspace.org-0001/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/df.mdgspace.org-0001/privkey.pem;
-    include /etc/letsencrypt/options-ssl-nginx.conf;
-    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
-    }"
+
 available_ports=()
 
 for ((port=PORT_MIN; port<=PORT_MAX; port++)); do
@@ -59,6 +38,27 @@ sudo rm Dockerfile
 sudo rm .env
 sudo touch /etc/nginx/sites-available/$2.conf
 sudo chmod 666 /etc/nginx/sites-available/$2.conf
-sudo echo $server_block > /etc/nginx/sites-available/$2.conf
+sudo echo "# Virtual Host configuration for $2
+    server {
+    listen 80;
+    listen [::]:80;
+    listen 443 ssl;
+    listen [::]:443 ssl;
+    server_name $2;
+    location / {
+        proxy_pass http://localhost:${available_ports[$AVAILABLE]};
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+    charset utf-8;
+    client_max_body_size 20M;
+    ssl_certificate /etc/letsencrypt/live/df.mdgspace.org-0001/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/df.mdgspace.org-0001/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+    }" > /etc/nginx/sites-available/$2.conf
 sudo ln -s /etc/nginx/sites-available/$2.conf /etc/nginx/sites-enabled/$2.conf
 sudo systemctl reload nginx
