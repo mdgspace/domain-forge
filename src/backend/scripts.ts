ba5e2dccf9911f431494f717f96b25ac4bbc9d9f
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2024-04-03 23:22:28 +0530

    feat: add memory limit to container

--- src/backend/scripts.ts
+++ src/backend/scripts.ts
@@ -2,6 +2,8 @@ import { exec } from "./dependencies.ts";
 import dockerize from "./utils/container.ts";
 import DfContentMap from "./types/maps_interface.ts";
 
+const MEMORY_LIMIT = Deno.env.get("MEMORY_LIMIT");
+
 async function addScript(
   document: DfContentMap,
   env_content: string,
@@ -21,14 +23,14 @@ async function addScript(
   } else if (document.resource_type === "GITHUB" && static_content == "Yes") {
     Deno.writeTextFile(`/hostpipe/.env`, env_content);
     await exec(
-      `bash -c "echo 'bash ../../src/backend/shell_scripts/container.sh -s ${document.subdomain} ${document.resource}' > /hostpipe/pipe"`,
+      `bash -c "echo 'bash ../../src/backend/shell_scripts/container.sh -s ${document.subdomain} ${document.resource} 80 ${MEMORY_LIMIT}' > /hostpipe/pipe"`,
     );
   } else if (document.resource_type === "GITHUB" && static_content == "No") {
     const dockerfile = dockerize(stack, port, build_cmds);
     Deno.writeTextFile(`/hostpipe/Dockerfile`, dockerfile);
     Deno.writeTextFile(`/hostpipe/.env`, env_content);
     await exec(
-      `bash -c "echo 'bash ../../src/backend/shell_scripts/container.sh -g ${document.subdomain} ${document.resource} ${port}' > /hostpipe/pipe"`,
+      `bash -c "echo 'bash ../../src/backend/shell_scripts/container.sh -g ${document.subdomain} ${document.resource} ${port} ${MEMORY_LIMIT}' > /hostpipe/pipe"`,
     );
   }
 }
