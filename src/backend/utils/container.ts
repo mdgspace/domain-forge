e336ad3be5c3631face4eb2309eeebd4af7997e0
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2024-04-16 18:45:11 +0530

    fix: fix dockerfile bug

--- src/backend/utils/container.ts
+++ src/backend/utils/container.ts
@@ -4,6 +4,7 @@ export default function dockerize(
   build_cmds: string,
 ) {
   let dockerfile = "";
+  build_cmds = build_cmds.replace(/\r?\n$/, '');
   const run_cmd = build_cmds.split("\n");
   const execute_cmd = "CMD " + JSON.stringify(run_cmd.pop()?.split(" "));
   const build_cmds_mapped = run_cmd.map((elem) => {
@@ -11,12 +12,12 @@ export default function dockerize(
   }).join("\n");
   if (stack == "Python") {
     dockerfile =
-      "FROM python:3.11 \nWORKDIR /app \nCOPY requirements.txt . \nRUN pip install --no-cache-dir -r requirements.txt \nCOPY . ." +
+      "FROM python:latest \nWORKDIR /app \nCOPY requirements.txt . \nRUN pip install --no-cache-dir -r requirements.txt \nCOPY . ." +
       build_cmds_mapped + `\nEXPOSE ${port}\n` + execute_cmd;
   } else if (stack == "NodeJS") {
     dockerfile =
-      "FROM node:latest \n WORKDIR /app \n COPY ./package*.json . \n RUN npm install \n COPY . ." +
-      build_cmds_mapped + `\n EXPOSE ${port} \n` + execute_cmd;
+      "FROM node:latest \nWORKDIR /app \nCOPY ./package*.json . \nRUN npm install \nCOPY . ." +
+      build_cmds_mapped + `\nEXPOSE ${port} \n` + execute_cmd;
   }
   return dockerfile.toString();
 }
